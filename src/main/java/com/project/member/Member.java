9c5b8a24546b3e91474c648c50486a2d1783244c
Justin Seaward <jut_seaward@yahoo.com>
2026-07-22 22:14:27 -0230

    added instance variables and getters and setters to the member class

--- src/main/java/com/project/member/Member.java
+++ src/main/java/com/project/member/Member.java
@@ -5,9 +5,96 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.time.LocalDate;
+
 @Entity
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private String firstName;
+    private String lastName;
+    private String mailingAddress;
+    private String email;
+    private String phoneNumber;
+    private LocalDate membershipStartDate;
+    private String membershipType;
+
+    public Member(){
+    }
+
+    public Member(long id, String firstName, String lastName, String mailingAddress, String email, String phoneNumber, LocalDate membershipStartDate, String membershipType) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mailingAddress = mailingAddress;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.membershipStartDate = membershipStartDate;
+        this.membershipType = membershipType;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMailingAddress() {
+        return mailingAddress;
+    }
+
+    public void setMailingAddress(String mailingAddress) {
+        this.mailingAddress = mailingAddress;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getMembershipStartDate() {
+        return membershipStartDate;
+    }
+
+    public void setMembershipStartDate(LocalDate membershipStartDate) {
+        this.membershipStartDate = membershipStartDate;
+    }
+
+    public String getMembershipType() {
+        return membershipType;
+    }
+
+    public void setMembershipType(String membershipType) {
+        this.membershipType = membershipType;
+    }
 }
