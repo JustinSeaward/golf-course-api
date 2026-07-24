65efd3d33d82a14b92138ffbd267dc804a7e3df9
Justin Seaward <jut_seaward@yahoo.com>
2026-07-24 11:35:00 -0230

    added member and tournament interface and extands repositories

--- src/main/java/com/project/tournament/TournamentRepository.java
+++ src/main/java/com/project/tournament/TournamentRepository.java
@@ -1,4 +1,6 @@
 package com.project.tournament;
 
-public class TournamentRepository {
+import org.springframework.data.repository.CrudRepository;
+
+public interface TournamentRepository extends CrudRepository<Tournament, Long> {
 }
