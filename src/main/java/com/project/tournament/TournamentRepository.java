edcb4152d0820d766da58d5d995f4a01f9cc4d97
Justin Seaward <jut_seaward@yahoo.com>
2026-07-27 19:47:11 -0230

    TournamentRespository: added methods to access database info

--- src/main/java/com/project/tournament/TournamentRepository.java
+++ src/main/java/com/project/tournament/TournamentRepository.java
@@ -1,6 +1,12 @@
 package com.project.tournament;
 
+import com.project.member.Member;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface TournamentRepository extends CrudRepository<Tournament, Long> {
+    List<Tournament> findTournamentByStartDate(LocalDate tournamentStartDate);
+    Tournament findTournamentByLocation(String location);
 }
