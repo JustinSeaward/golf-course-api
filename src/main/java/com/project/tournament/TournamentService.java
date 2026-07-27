c41568c788d8c0f1b98aa1ff5cb8bdaee30ce882
Justin Seaward <jut_seaward@yahoo.com>
2026-07-27 19:48:27 -0230

    TournamentService: added methods with logic to return data for the controller methods

--- src/main/java/com/project/tournament/TournamentService.java
+++ src/main/java/com/project/tournament/TournamentService.java
@@ -1,7 +1,30 @@
 package com.project.tournament;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class TournamentService {
+
+    @Autowired
+    private TournamentRepository tournamentRepository;
+
+    public Tournament createTournament(Tournament tournament){
+        return tournamentRepository.save(tournament);
+    }
+
+    public List<Tournament> getAllTournaments(){
+        return (List<Tournament>) tournamentRepository.findAll();
+    }
+
+    public Tournament getTournamentByLocation(String location) {
+        return tournamentRepository.findTournamentByLocation(location);
+    }
+
+    public List<Tournament> getTournamentByStartDate(LocalDate startDate){
+        return tournamentRepository.findTournamentByStartDate(startDate);
+    }
 }
