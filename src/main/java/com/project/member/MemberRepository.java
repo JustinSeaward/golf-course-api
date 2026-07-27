9eb159eb5561d56317bd8e8c1174b1d14e4ca102
Justin Seaward <jut_seaward@yahoo.com>
2026-07-27 20:24:00 -0230

    MemberRepository: using JAP instand of CRUD and renamed tournamentStartDate permeter to startDate

--- src/main/java/com/project/member/MemberRepository.java
+++ src/main/java/com/project/member/MemberRepository.java
@@ -1,13 +1,13 @@
 package com.project.member;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public interface MemberRepository extends CrudRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findMemberByName(String name);
     List<Member> findMemberByMembershipType(String membershipType);
     Member findMemberByPhoneNumber(String phoneNumber);
-    List<Member> findMemberByTournamentStartDate(LocalDate tournamentStartDate);
+    List<Member> findMemberByTournamentStartDate(LocalDate startDate);
 }
